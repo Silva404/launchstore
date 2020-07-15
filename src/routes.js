dc2becd164abb9299f549dc17c78a0ec6a102515
Silva404 <brenosilva0252@gmail.com>
2020-07-15 17:20:00 -0300

    init

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/',(req, res) => {
-    return res.send('oi')
+    return res.render('layout.njk')
 })
 
 module.exports = routes
