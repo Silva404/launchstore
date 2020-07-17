8af49d902428f1fa42ed83db76deb341ada590a6
Silva404 <brenosilva0252@gmail.com>
2020-07-17 15:13:33 -0300

    pagination

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const routes = express.Router()
+const ProductController = require('./app/controllers/ProductController')
 
 routes.get('/',(req, res) => {
     return res.render('layout.njk')
 })
+
+routes.get('/products/create', ProductController.create)
+
+routes.get('/ads/create',(req, res) => {
+    return res.rendirect('/products/create')
+})  
 
 module.exports = routes
