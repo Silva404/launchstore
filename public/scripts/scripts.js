343de1dc8f49477a05c8ee76d54e01ca93649a19
Silva404 <brenosilva0252@gmail.com>
2020-07-18 16:11:12 -0300

    pagination

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,4 +1,31 @@
-const input = document.querySelector('input[type=text]')
-input.addEventListener('keydown', e => {
-  console.log(e)
-})
+// const input = document.querySelector('input[name=price]')
+// input.addEventListener('keydown', e => {
+//   setTimeout(() => {
+//     let { value } = e.target
+
+//     value = value.replace(/\D/g, '')
+
+//     value = new Intl.NumberFormat('pt-br', {
+//       style: 'currency',
+//       currency: 'BRL'
+//     }).format(value/100)
+
+//     e.target.value = value
+//   }, 1)
+// })
+
+const Mask = {
+  apply(input, func){
+    setTimeout(() => {
+      input.value = Mask[func](input.value)
+    }, 1)
+  },
+  convertBRL(value){
+    value = value.replace(/\D/g, '')
+
+    value = new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(value/100)
+  }
+}
