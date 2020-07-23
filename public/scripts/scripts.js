26a7af7f3582f1f41809f5b9b7aeac6d6becf4a6
Silva404 <brenosilva0252@gmail.com>
2020-07-23 12:17:21 -0300

    images manager

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -14,82 +14,102 @@ const Mask = {
   }
 }
 
+// const PhotosUpload = {
+//   input: '',
+//   preview: document.querySelector('#photos-preview'),
+//   files: [],
+//   uploadLimit: 6,
+//   handleFileInput(event) {
+//     const { files: fileList } = event.target    
+//     PhotosUpload.input = event.target
+
+//     if (PhotosUpload.hasLimit(event)) return
+
+//     Array.from(fileList).forEach(file => {
+//       const reader = new FileReader()
+
+//       PhotosUpload.files.push(file)
+//       console.log(this.files)
+
+//       reader.onload = () => {
+//         const image = new Image()
+//         image.src = String(reader.result)
+
+//         const div = PhotosUpload.getContainer(image)
+
+//         PhotosUpload.preview.appendChild(div)
+//       }
+
+//       reader.readAsDataURL(file)
+//     })
+
+//     PhotosUpload.input.files = PhotosUpload.getAllFiles()
+//   },
+//   hasLimit(event) {
+//     const { uploadLimit, input, preview } = PhotosUpload
+//     const { files: fileList } = input
+
+//     if (fileList.length > uploadLimit) {
+//       alert(`Selecione no máximo ${uploadLimit} fotos`)
+//       event.preventDefault()
+//       return true
+//     }
+
+//     const photosDiv = []
+//     preview.childNodes.forEach(item => {
+//       if (item.classList && item.classList.value == 'photo') {
+//         photosDiv.push(item)
+//       }
+//     })
+
+//     const totalPhotos = fileList.length + photosDiv.length
+
+//     if (totalPhotos > uploadLimit) {
+//       alert(`Você excedeu o limite de fotos.`)
+//       event.preventDefault()
+//       return true
+//     }
+
+
+//     return false
+//   },
+//   getAllFiles() {
+//     const dataTransfer = new ClipboardEvent('').clipboardData || new DataTransfer()
+
+//     PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+//     return dataTransfer.files
+//   },
+//   getContainer(image) {
+//     const div = document.createElement('div')
+//     div.classList.add('photo')
+//     div.onclick = this.removePhoto
+//     div.appendChild(image)
+//     div.appendChild(this.getRemoveButton())
+
+//     return div
+//   },
+//   getRemoveButton() {
+//     const remove = document.createElement('i')
+//     remove.classList.add('material-icons')
+//     remove.innerHTML = 'close'
+
+//     return remove
+//   },
+//   removePhoto(event) {
+//     // event.target = i e um item acima dele = img
+//     const photoDiv = event.target.parentNode
+//     const photosArray = Array.from(PhotosUpload.preview)
+
+//     const index = photosArray.indexOf(photoDiv)
+//     PhotosUpload.files.splice(index, 1)
+//     PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+
+//     photoDiv.remove()
+//   }
+// }
+
 const PhotosUpload = {
-  input: '',
-  preview: document.querySelector('#photos-preview'),
-  files: [],
-  uploadLimit: 6,
-  handleFileInput(event) {
-    const { files: fileList } = event.target    
-    PhotosUpload.input = event.target
-
-    if (PhotosUpload.hasLimit(event)) return
-
-    Array.from(fileList).forEach(file => {
-      const reader = new FileReader()
-
-      PhotosUpload.files.push(file)
-      console.log(this.files)
-
-      reader.onload = () => {
-        const image = new Image()
-        image.src = String(reader.result)
-
-        const div = PhotosUpload.getContainer(image)
-
-        PhotosUpload.preview.appendChild(div)
-      }
-
-      reader.readAsDataURL(file)
-    })
-
-    PhotosUpload.input.files = PhotosUpload.getAllFiles()
-  },
-  hasLimit(event) {
-    const { uploadLimit, input: fileList } = PhotosUpload
-    // const { files: fileList } = event.target
-
-    if (fileList.length > uploadLimit) {
-      alert(`Selecione no máximo ${uploadLimit} fotos`)
-      event.preventDefault()
-      return true
-    }
-
-    return false
-  },
-  getAllFiles() {
-    const dataTransfer = new ClipboardEvent('').clipboardData || new DataTransfer()
-
-    PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
-
-    return dataTransfer.files
-  },
-  getContainer(image) {
-    const div = document.createElement('div')
-    div.classList.add('photo')
-    div.onclick = this.removePhoto
-    div.appendChild(image)
-    div.appendChild(this.getRemoveButton())
-
-    return div
-  },
-  getRemoveButton() {
-    const remove = document.createElement('i')
-    remove.classList.add('material-icons')
-    remove.innerHTML = 'close'
-
-    return remove
-  },
-  removePhoto(event) {
-    // event.target = i e um item acima dele = img
-    const photoDiv = event.target.parentNode
-    const photosArray = Array.from(PhotosUpload.preview)
-
-    const index = photosArray.indexOf(photoDiv)
-    PhotosUpload.files.splice(index, 1)
-    PhotosUpload.input.files = PhotosUpload.getAllFiles()
-
-
-    photoDiv.remove()
-  }
+  
 }
