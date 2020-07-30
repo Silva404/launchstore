3bc4a40c465285606b05baa7a5cfc1b173326e2b
Silva404 <brenosilva0252@gmail.com>
2020-07-30 19:56:32 -0300

    i can finally delete everything

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,6 +1,5 @@
 const { formatPrice } = require('../../lib/utils')
 
-const File = require('../models/File')
 const Product = require('../models/Product')
 
 module.exports = {
@@ -12,12 +11,24 @@ module.exports = {
     if (!products) return res.send('Products not found!')
 
     async function getImage(productId) {
-      let results = await Product.files()
-      let files = results.rows
-      files = files.map(file => Product.files(products))
+      let results = await Product.files(product.Id)
+      const files = results.rows.map(file => 
+        `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+      )
 
+      return files[0]
     }
 
-    return res.render('home/index')
+    const productsPromise = products.map(async product => {
+      product.image = await getImage(product)
+      product.oldPrice = formatPrice(product.old_price)
+      product.price = formatPrice(product.price)
+
+      return product
+    }).filter((product, index) => index > 2 ? false : true)
+
+    const eachProductFixed = await Promise.all(productsPromise)
+
+    return res.render('home/index', { products: eachProductFixed })
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 const ProductController = require('./app/controllers/ProductController')
+const HomeController = require('./app/controllers/HomeController')
 const multer = require('./app/middlewares/multer')
 
 routes.get('/',(req, res) => {
-    return res.render('home/index.njk')
+    return res.render('home/index.njk', HomeController.index)
 })
 
 routes.get('/products/create', ProductController.create)
