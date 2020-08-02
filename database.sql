ef2b6fc09d5bd6b8733eb53a12080d0f5fa2b003
Silva404 <brenosilva0252@gmail.com>
2020-08-02 14:49:51 -0300

    recipes refatorada

--- database.sql
+++ database.sql
@@ -9,23 +9,23 @@ CREATE TABLE "products" (
   "description" text NOT NULL,
   "old_price" int,
   "price" int NOT NULL,
-  "quantity" int DEEAFULT 0,
+  "quantity" int DEFAULT 0,
   "status" int DEFAULT 1,
   "created_at" timestamp DEFAULT (now()),
-  "updated_at" timestamp DEFAULT (now()
+  "updated_at" timestamp DEFAULT (now())
 );
   
 CREATE TABLE "categories" (
 	"id" SERIAL PRIMARY KEY,
   "name" text NOT NULL
-)
+);
     
 CREATE TABLE "files" (
 	"id" SERIAL PRIMARY KEY,
   "name" text NOT NULL,
-  "name" text NOT NULL,
+  "path" text NOT NULL,
   "product_id" int NOT NULL
-)
+);
 
 CREATE TABLE "users" (
 	"id" SERIAL PRIMARY KEY,
@@ -33,7 +33,7 @@ CREATE TABLE "users" (
   "email" text UNIQUE NOT NULL,
   "password" text NOT NULL,
   "cpf_cnpj" int UNIQUE NOT NULL,
-  "cpef" text,
+  "cep" text,
   "address" text,
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
@@ -44,19 +44,24 @@ ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
 ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
 CREATE FUNCTION trigger_set_timestamp()
-RETURN TRIGGER AS $$
+RETURNS TRIGGER AS $$
 BEGIN
-  NEW.updated.at = NOW ();
-  RETURN NEW;
+  NEW.updated_at = NOW ();
+  RETURN NEW;	
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER set_timestamp
-BEFORE UPDATED ON products
+BEFORE UPDATE ON products
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 CREATE TRIGGER set_timestamp
-BEFORE UPDATED ON users
+BEFORE UPDATE ON users
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
+
+INSERT INTO categories(name) VALUES('Eletrônicos');
+INSERT INTO categories(name) VALUES('Roupas');
+INSERT INTO categories(name) VALUES('Móveis');
+INSERT INTO categories(name) VALUES('Comida');
