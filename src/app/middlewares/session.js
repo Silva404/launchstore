c32ea51bfbe9f905e87875e1b34ace760499d271
Silva404 <brenosilva0252@gmail.com>
2020-08-05 13:24:57 -0300

    validation

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -1,9 +1,14 @@
 function onlyUsers(req, res, next) {
   if (!req.session.userId) return res.redirect('/users/login')
+  next()
+}
 
+function ifLoggedRedirectToUsers(req, res, next) {
+  if (req.session.userId) return res.redirect('/users')
   next()
 }
 
 module.exports = {
-  onlyUsers
+  onlyUsers,
+  ifLoggedRedirectToUsers
 }
