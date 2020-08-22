d5814f888035f42bc0f15aafdcd6c5a395f14d8d
Silva404 <brenosilva0252@gmail.com>
2020-08-22 18:08:26 -0300

    refactoring: base models

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,3 +1,51 @@
+const Base = require("./Base");
+Base.init({ table: "products" });
+
+module.exports = {
+  ...Base,
+  async files(id) {
+    const results = await db.query(`SELECT * FROM files WHERE product_id = $1`, [id])
+  
+    return results.rows
+  },
+  async search(params) {
+    const { filter, category } = params
+  
+    let query = '',
+      filterQuery = 'WHERE'
+  
+    if (category) {
+      filterQuery = `
+      ${filterQuery}
+      products.category_id = ${category}
+      AND
+      `
+    }
+  
+    filterQuery = `
+      ${filterQuery}
+      products.name ilike '%${filter}%'
+      OR products.description ilike '%${filter}%'
+    `
+  
+    query = `
+        SELECT products.*, 
+        categories.name AS category_name
+        FROM products 
+        LEFT JOIN categories ON (categories.id = products.category_id)
+        ${filterQuery}
+      `
+  
+    const results = await db.query(query)
+  
+    return results.rows
+  }
+}
+
+
+
+
+
 // module.exports = {
 //   async all() {
 //     const results = await db.query(`
@@ -113,48 +161,3 @@
 //     return results.rows
 //   }
 // }
-
-const Base = require("./Base");
-Base.init({ table: "products" });
-
-const User = {
-  ...Base,
-  async files(id) {
-    const results = await db.query(`SELECT * FROM files WHERE product_id = $1`, [id])
-  
-    return results.rows
-  },
-  async search(params) {
-    const { filter, category } = params
-  
-    let query = '',
-      filterQuery = 'WHERE'
-  
-    if (category) {
-      filterQuery = `
-      ${filterQuery}
-      products.category_id = ${category}
-      AND
-      `
-    }
-  
-    filterQuery = `
-      ${filterQuery}
-      products.name ilike '%${filter}%'
-      OR products.description ilike '%${filter}%'
-    `
-  
-    query = `
-        SELECT products.*, 
-        categories.name AS category_name
-        FROM products 
-        LEFT JOIN categories ON (categories.id = products.category_id)
-        ${filterQuery}
-      `
-  
-    const results = await db.query(query)
-  
-    return results.rows
-  }
-}
-
